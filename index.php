429e90aec86e8e813a71d9ab786c057018989a42
DJOhayo <waga28@vp.pl>
2023-03-15 03:07:26 +0100

    Update Bootstrap to 5.3.0-alpha1 version

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap -->
-    <!--<link rel="stylesheet" href="css/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="css/bootstrap.rtl.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    -->
     <title>ZSZ Hajnówka</title>
 </head>
 <body class="bg-light-subtle d-flex flex-column min-vh-100" data-bs-theme="dark">
